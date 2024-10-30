8cf385e76a4365b3d896a2346387f03baf8c1d37
Anthony Loeza <aloeza1@cps.edu>
2024-10-30 11:34:17 -0500

    worked through notes

--- notes.py
+++ notes.py
@@ -15,10 +15,7 @@ class Dog:
 
     def __str__(self) -> str:
         """Return a string representation of a dog"""
-        s = "Dog's name is " + self.name + "\n"
-        s += "and age is " + str(self.age) + "\n"
-        s += "and fur color is " + self.fur_color + "\n"
-        s += "they have played fetch " + str(self.fetch_count) + " times\n"
+        s = f"{self.name} is {self.age} years old"
         return s
 
     def play_fetch(self, num_times):
@@ -27,6 +24,7 @@ class Dog:
     def paint_dog(self, color):
         self.fur_color = color
 
+#Instances of the dog class
 mydog = Dog("logan", "black", 7, "salmon")
 chrisdog = Dog("luna", "black and white", 6, "tortillas")
 
